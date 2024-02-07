3895fad1f24b2f54a7a0b8d9ace3c2b4763f2244
Natalia Soares <nataliasilvamvn@gmail.com>
2024-02-07 20:09:03 -0300

    Aprendendo sobre Rotas no Angular [DIO]

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,14 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterOutlet } from '@angular/router';
+import { IndexModule } from './pages/index/index.module';
+import { PortifolioModule } from './pages/portifolio/portifolio.module';
+import { MenuComponent } from './shared/menu/menu.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [CommonModule, RouterOutlet],
+  imports: [ MenuComponent, PortifolioModule,IndexModule, CommonModule, RouterOutlet],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,18 @@
 import { Routes } from '@angular/router';
+import { TitleComponent } from './pages/index/title/title.component';
+import { CardComponent } from './pages/portifolio/card/card.component';
 
-export const routes: Routes = [];
+//Craindo rotas e passando o caminho
+//Routes pede para trabalhar com objetos
+
+//Caminho (path) vazio para abrir na pagina inicial e vai abrir o Component Title
+//pathMatch - ''prefix' a URL precisa ter o caminho e pode ter outras coisas ou 'full' URL exata, não pode ter variação dela
+//Para URL principal coloca 'full'
+export const routes: Routes = [
+  {path:'', component: TitleComponent, pathMatch:'full'},
+  {path:'portifolio',component: CardComponent},
+
+  //Rota Coriga aceita qualquer coisa com caminho - 'redirectTo' redireciona para outra rota
+  //Se digitar qualquer coisa que não existe volta para home
+  {path:'**', redirectTo:''}
+];
