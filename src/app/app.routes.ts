05e6aafc8e627a21423d975a8afa74f8c2aaa893
Natalia Soares <nataliasilvamvn@gmail.com>
2024-02-21 19:42:36 -0300

    Pegando informações da URL e Criando Rotas Children (Filhas)

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -8,9 +8,15 @@ import { CardComponent } from './pages/portifolio/card/card.component';
 //Caminho (path) vazio para abrir na pagina inicial e vai abrir o Component Title
 //pathMatch - ''prefix' a URL precisa ter o caminho e pode ter outras coisas ou 'full' URL exata, não pode ter variação dela
 //Para URL principal coloca 'full'
+
 export const routes: Routes = [
   {path:'', component: TitleComponent, pathMatch:'full'},
-  {path:'portifolio',component: CardComponent},
+
+  //Criando Rotas filhas (Children), pode passar um Vetor dentro de ' children '
+  {path:'portifolio',component: CardComponent, children:[
+    {path:':id',component: CardComponent}
+  ]},
+
 
   //Rota Coriga aceita qualquer coisa com caminho - 'redirectTo' redireciona para outra rota
   //Se digitar qualquer coisa que não existe volta para home
--- src/app/pages/portifolio/card/card.component.ts
+++ src/app/pages/portifolio/card/card.component.ts
@@ -1,4 +1,6 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute, Router } from '@angular/router';
+
 
 @Component({
   selector: 'app-card',
@@ -7,6 +9,47 @@ import { Component } from '@angular/core';
   templateUrl: './card.component.html',
   styleUrl: './card.component.css'
 })
-export class CardComponent {
+
+export class CardComponent implements OnInit {
+
+  constructor(private parametro: ActivatedRoute, private navegador: Router){
+
+    //Recuperando a informação do Id passado na URL
+    this.parametro.params.subscribe(
+      res => console.log(res)
+    )
+
+     //Recuperando os QueryParams (parametros de Consulta) da URL
+    //Recuperando da URL http://localhost:4200/portifolio/1?name=Natalia&token=123
+    //variavel será igual {name:'Natalia', token:'123'}
+
+    this.parametro.queryParams.subscribe(
+      variavel => console.log(variavel)
+    )
+
+    //Recuperando parametro da Rota Children (Filha de Portifolio)
+    //Só colocar 'firstChild' a '?' é pq pode não existir
+    this.parametro.firstChild?.params.subscribe(
+      res => console.log(res)
+    )
+
+    //Rota Children (FILHA) Parametros de Consulta
+    this.parametro.firstChild?.queryParams.subscribe(
+      variavel => console.log(variavel)
+    )
+  }
+
+  //Colocando um tempo para ficar na pagina Portifolio
+  //Depois de 5 seg redireciona para outra Pagina
+  ngOnInit(): void {
+
+    //o ''navigate' permite ir para uma Rota, no caso indo pra Home
+    //setInterval(()=>{
+   //   this.navegador.navigate(['/'])
+   // },5000)
+  }
+
 
 }
+
+
